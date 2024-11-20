c9f8c9d49ff1044093a0e81703a6c2405feb30ca
Michael Gorbunov <mg0rby@yandex.ru>
2024-11-20 15:57:22 +0300

    30.2 часть 8

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -9,7 +9,7 @@ class MilageSerializer(serializers.ModelSerializer):
 
 class CarSerializer(serializers.ModelSerializer):
     last_milage = serializers.IntegerField(source="milage_set.all.first.milage")
-    milage= MilageSerializer(source="milage_set",many=True)
+    milage= MilageSerializer(many=True)
 
     class Meta:
         model= Car
@@ -21,13 +21,13 @@ class MotoSerializer(serializers.ModelSerializer):
         model= Moto
         fields ="__all__"
     def get_last_milage(self,instance):
-        # if instance.milage_set.all().first():
-        #     return instance.milage_set.all().first().milage
-        # return 0
-        milage = instance.milage_set.first()#сразу первое значение
-        if not milage:
-            return 0
-        return milage.milage
+        if instance.milage.all().first():
+            return instance.milage.all().first().milage
+        return 0
+        # milage = instance.milage_set.first()#сразу первое значение
+        # if not milage:
+        #     return 0
+        # return milage.milage
 
 class MotoMilageSerializer(serializers.ModelSerializer):
     moto = MotoSerializer()
@@ -35,3 +35,16 @@ class MotoMilageSerializer(serializers.ModelSerializer):
         model = Milage
         fields=("milage","year","moto",)
     # fields = ("milage", "year", )
+
+class MotoCreateSerializer(serializers.ModelSerializer):
+    milage=MilageSerializer(many=True)
+    class Meta:
+        model=Moto
+        fields ="__all__"
+    def create(self, validated_data):
+        milage=validated_data.pop("milage")
+        moto_item=Moto.objects.create(**validated_data)
+
+        for m in milage:
+            Milage.objects.create(**m,moto=moto_item)
+            return moto_item
